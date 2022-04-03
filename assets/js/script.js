ce3cb5762b30a7349c58116d125c9a2afd4c6fbb
Auzzie1790 <Auzzie1790@gmail.com>
2022-04-03 17:12:58 -0400

    save goes to localstorage and displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-// Display today's day and date
+// Display Date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+// save button listenter
+$(".saveBtn").on("click", function() {
+// check text value
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id"); 
+
+// save in local storage
+    localStorage.setItem(time, value);
+})
+
+// retreive from local storage
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
