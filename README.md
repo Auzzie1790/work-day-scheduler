2d0089ab1fd0f8157646980de3e6f6bfd45221ec
Auzzie1790 <Auzzie1790@gmail.com>
2022-04-03 23:06:54 -0400

    color matching finished. ReadMe started

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# work-day-scheduler
+# Work Day Schedular
+
+## How to use 
+Click on the box to the corasponding hour you want to add a task to. Once the task has been added click the save button on the right. 
+
+## Color Key
+Grey = Past
+Red = Current
+Green = Future
+
+![Website Picture](assets/images/Screen Shot 2022-04-03 at 3.02.29 PM.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,99 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+var hour8 = 8;
+var hour9 = 9;
+var hour10 = 10;
+var hour11 = 11;
+var hour12 = 12;
+var hour13 = 13;
+var hour14 = 14;
+var hour15 = 15;
+var hour16 = 16;
+var hour17 = 17;
+
+//Change textarea background color based on time
+var currentHour = moment().format('H');
+
+
+if (currentHour < hour8) {
+    $("#hour8").addClass("future");
+} else if (currentHour > hour8) {
+    $("#hour8").addClass("past");
+} else if (currentHour = hour8) {
+    $("#hour8").addClass("present");
+}
+
+if (currentHour < hour9) {
+    $("#hour9").addClass("future");
+} else if (currentHour > hour9) {
+    $("#hour9").addClass("past");
+} else if (currentHour = hour9) {
+    $("#hour9").addClass("present");
+}
+
+if (currentHour < hour10) {
+    $("#hour10").addClass("future");
+} else if (currentHour > hour10) {
+    $("#hour10").addClass("past");
+} else if (currentHour = hour10) {
+    $("#hour10").addClass("present");
+}
+
+if (currentHour < hour11) {
+    $("#hour11").addClass("future");
+} else if (currentHour > hour11) {
+    $("#hour11").addClass("past");
+} else if (currentHour = hour11) {
+    $("#hour11").addClass("present");
+}
+
+if (currentHour < hour12) {
+    $("#hour12").addClass("future");
+} else if (currentHour > hour12) {
+    $("#hour12").addClass("past");
+} else if (currentHour = hour12) {
+    $("#hour12").addClass("present");
+}
+
+if (currentHour < hour13) {
+    $("#hour13").addClass("future");
+} else if (currentHour > hour13) {
+    $("#hour13").addClass("past");
+} else if (currentHour = hour13) {
+    $("#hour13").addClass("present");
+}
+
+if (currentHour < hour14) {
+    $("#hour14").addClass("future");
+} else if (currentHour > hour14) {
+    $("#hour14").addClass("past");
+} else if (currentHour = hour14) {
+    $("#hour14").addClass("present");
+}
+
+if (currentHour < hour15) {
+    $("#hour15").addClass("future");
+} else if (currentHour > hour15) {
+    $("#hour15").addClass("past");
+} else if (currentHour = hour15) {
+    $("#hour15").addClass("present");
+}
+
+if (currentHour < hour16) {
+    $("#hour16").addClass("future");
+} else if (currentHour > hour16) {
+    $("#hour16").addClass("past");
+} else if (currentHour = hour16) {
+    $("#hour16").addClass("present");
+}
+
+if (currentHour < hour17) {
+    $("#hour17").addClass("future");
+} else if (currentHour > hour17) {
+    $("#hour17").addClass("past");
+} else if (currentHour = hour17) {
+    $("#hour17").addClass("present");
+}
+
